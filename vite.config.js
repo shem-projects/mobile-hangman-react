3b19286a8b98713803e76020bd2afc9ffe487ba5
shem-projects <shemmalus@gmail.com>
2025-01-19 17:48:49 -0500

    Update Vite configuration to fix build issues

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   resolve: {
     alias: {
