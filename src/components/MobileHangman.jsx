46abd9046b4bb297315f0b38b9c47a4fabebcef4
shem-projects <shemmalus@gmail.com>
2025-01-19 15:47:01 -0500

    Add MobileHangman component

---
+++ src/components/MobileHangman.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { Card, CardContent } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { 
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+
+// ... [Rest of the working MobileHangman component code] ...
