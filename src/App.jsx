9f030f9343cf47bcd4f0f2d3cbfa395e9a14e9e4
shem-projects <shemmalus@gmail.com>
2025-01-19 15:50:56 -0500

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MobileHangman from './components/MobileHangman';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-zinc-950 p-4">
+      <MobileHangman />
+    </div>
+  );
+}
+
+export default App;
